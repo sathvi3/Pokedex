d6638da5eddf78b32d5fca13511b4466733e4a5c
sathvi3 <sathvikbhandar007@hotmail.com>
2020-08-22 21:35:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+import backgroundImage from './pattern.png';
+import Pokemon from './components/pokemon/Pokemon';
+
+function App() {
+  return (
+    <Router>
+      <div className="App" style={{background: `url(${backgroundImage})` }}>
+        <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+              <Dashboard />
+            </Switch>
+          </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return ( 
+            <div>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                    <a href=" " className="navbar-brand col-sm-3 col-md-2 mr-o align-items-center"> 
+                    Pokedex 
+                    </a>
+                </nav>
+            </div>
+         );
+    }
+}
+ 
+export default NavBar;
